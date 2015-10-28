19657b7bc219b226243773cef48adb6bc9f75860
Jose Pleonasm <jose.pleonasm@gmail.com>
2015-10-28 11:04:43 +0100

    better handler mock

--- test/mocks.js
+++ test/mocks.js
@@ -1,5 +1,10 @@
+var util = require('util');
+var Handler = require('../lib/logging').Handler;
 
-function MockHandler() {}
+function MockHandler() {
+	Handler.call(this);
+}
+util.inherits(MockHandler, Handler);
 
 MockHandler.prototype.handle = function(record) {};
 
