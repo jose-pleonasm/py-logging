2ed0dc2562d4f8a934214b9f7a6145b5c3cf5204
Jose Pleonasm <jose.pleonasm@gmail.com>
2019-03-26 19:18:01 +0100

    2.4.0

--- core/logging.js
+++ core/logging.js
@@ -11,7 +11,7 @@ var Manager = require('./Manager');
  */
 
 var MODULE_IDENTIFIER = 'logging';
-var VERSION = '2.3.0';
+var VERSION = '2.4.0';
 
 
 //------------------------------------------------------------------------------
